b724a9b2490b6db135e71cb73e2f8d4fc322ee1c
Barbaros Teoman Kosoglu <btkkteoman@icloud.com>
2023-07-15 01:48:01 +0300

    update

--- src/components/Tab3.tsx
+++ src/components/Tab3.tsx
@@ -1,22 +1,20 @@
 export default function Tab3() {
-  return (
-    <div className="tabsMain">
-      <div className="tabs" style={{ marginTop: "min(5%, 30px)" }}>
-        <div className="tabsLeft">
-          <h1>Share your bookmarks</h1>
-          <p>
-            Easily share your bookmarks and collections with others. Create a
-            shareable link that you can send at the click of a button.
-          </p>
-          <div className="tabsLeftButtons">
-            <button>More Info</button>
-          </div>
-        </div>
-        <div className="tabsRight">
-          <img src="src/assets/illustration-features-tab-3.svg" alt="" />
-          <div className="elipse" />
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div className="tabs" style={{ marginTop: "min(5%, 30px)" }}>
+			<div className="tabsLeft">
+				<h1>Share your bookmarks</h1>
+				<p>
+					Easily share your bookmarks and collections with others. Create a
+					shareable link that you can send at the click of a button.
+				</p>
+				<div className="tabsLeftButtons">
+					<button>More Info</button>
+				</div>
+			</div>
+			<div className="tabsRight">
+				<img src="src/assets/illustration-features-tab-3.svg" alt="" />
+				<div className="elipse" />
+			</div>
+		</div>
+	)
 }
