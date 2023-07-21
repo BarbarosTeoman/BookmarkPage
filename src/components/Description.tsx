477bae6a16ca08e76939c7c7165070a356caaa62
Barbaros Teoman Kosoglu <btkkteoman@icloud.com>
2023-07-21 17:06:23 +0300

    update

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,21 +1,22 @@
+import illustrationHero from "../assets/illustration-hero.svg"
 export default function Description() {
-  return (
-    <div className="description">
-      <div className="descriptionLeft">
-        <h1>A Simple Bookmark Manager</h1>
-        <p>
-          A clean and simple interface to organize your favourite websites. Open
-          a new browser tab and see your sites load instantly. Try it for free.
-        </p>
-        <div className="descriptionLeftButtons">
-          <button>Get it on Chrome</button>
-          <button>Get it on Firefox</button>
-        </div>
-      </div>
-      <div className="descriptionRight">
-        <img src="src/assets/illustration-hero.svg" alt="" />
-        <div className="elipse" />
-      </div>
-    </div>
-  )
+	return (
+		<div className="description">
+			<div className="descriptionLeft">
+				<h1>A Simple Bookmark Manager</h1>
+				<p>
+					A clean and simple interface to organize your favourite websites. Open
+					a new browser tab and see your sites load instantly. Try it for free.
+				</p>
+				<div className="descriptionLeftButtons">
+					<button>Get it on Chrome</button>
+					<button>Get it on Firefox</button>
+				</div>
+			</div>
+			<div className="descriptionRight">
+				<img src={illustrationHero} alt="" />
+				<div className="elipse" />
+			</div>
+		</div>
+	)
 }
