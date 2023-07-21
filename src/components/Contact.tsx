847df3eb88f993046aac780652b3687d897c7ec9
Barbaros Teoman Kosoglu <btkkteoman@icloud.com>
2023-07-21 03:02:46 +0300

    update

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -35,34 +35,37 @@ export default function Contact() {
 						{message && (
 							<>
 								{error && (
-									<img
-										src="src\assets\icon-error.svg"
-										alt=""
-									/>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="20"
+										height="20"
+									>
+										<g fill="none" fill-rule="evenodd">
+											<circle cx="10" cy="10" r="10" fill="#FA5959" />
+											<g fill="#FFF" transform="translate(9 5)">
+												<rect width="2" height="7" rx="1" />
+												<rect width="2" height="2" y="8" rx="1" />
+											</g>
+										</g>
+									</svg>
 								)}
 								<div
 									style={
 										error
 											? {
-													backgroundColor:
-														"var(--softRed)",
+													backgroundColor: "var(--softRed)",
 											  }
 											: {
-													backgroundColor:
-														"var(--veryDarkBlue)",
+													backgroundColor: "var(--veryDarkBlue)",
 											  }
 									}
 									className="messageDiv"
 								>
 									<span>
 										{error ? (
-											<em>
-												Whoops, make sure it's an email
-											</em>
+											<em>Whoops, make sure it's an email</em>
 										) : (
-											<em>
-												You've successfully subscribed
-											</em>
+											<em>You've successfully subscribed</em>
 										)}
 									</span>
 								</div>
